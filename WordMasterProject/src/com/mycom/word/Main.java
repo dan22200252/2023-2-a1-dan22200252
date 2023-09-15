3a5358b75bc11b79dfec7479813adaecdda18db1
dan22200252 <22200252@handong.ac.kr>
2023-09-16 03:09:57 +0900

    오타 수정

--- WordMasterProject/src/com/mycom/word/Main.java
+++ WordMasterProject/src/com/mycom/word/Main.java
@@ -5,8 +5,6 @@ package com.mycom.word;
 public class Main {
     public static void main(String[] args) {
         WordManager wordManager = new WordManager();
-
         wordManager.start();
-
     }
 }
